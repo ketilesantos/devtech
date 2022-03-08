aec3741166a26fd76452ba50cb0d88e70a01c2df
Ketile Santos <ketile.pimenta@gmail.com>
2022-03-07 21:43:18 -0300

    Implementado layout do usuário logado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, IconButton } from '@mui/material';
+import { AppBar, Toolbar, Typography, IconButton, Grid, Avatar } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 
 interface HeaderProps {
@@ -14,9 +14,14 @@ export const Header = ({ onClick, mobile }: HeaderProps): JSX.Element => {
         <IconButton sx={{ display: mobile ? 'block' : 'none' }} aria-label="open drawer" onClick={onClick} edge="start">
           <MenuIcon sx={{ color: 'white' }} />
         </IconButton>
-        <Typography variant="h6" noWrap component="div">
-          ToDoList
-        </Typography>
+        <Grid container alignItems="center" justifyContent="flex-end">
+          <Typography sx={{ color: 'white', mr: '1rem' }}>Gabriel Silva</Typography>
+          <Avatar
+            alt="Photo User"
+            src="https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8YXZhdGFyfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            sx={{ border: '2px solid white' }}
+          />
+        </Grid>
       </Toolbar>
     </AppBar>
   );
